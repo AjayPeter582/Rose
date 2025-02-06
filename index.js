3683e53c2ef8d27f9afbe4a358d9a4e2a8d5b065
AjayPeter582 <128886618+AjayPeter582@users.noreply.github.com>
2025-02-06 22:58:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener("mousemove", function (e) {
+    let body = document.querySelector("body");
+    let flower = document.createElement("div");
+    flower.className = "mouse";
+  
+    let x = e.pageX;
+    let y = e.pageY;
+  
+    flower.style.left = x + "px";
+    flower.style.top = y + "px";
+  
+    let size = Math.random() * 80;
+    flower.style.width = 20 + size + "px";
+    flower.style.height = 20 + size + "px";
+  
+    let rotation = Math.random() * 360;
+    flower.style.transform = `rotate(${rotation}deg)`;
+  
+    body.appendChild(flower);
+  
+    setTimeout(function () {
+      flower.remove();
+    }, 9000);
+  });
